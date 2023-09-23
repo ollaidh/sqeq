56c9c958e6b144cba93c0ed31923026149bbc56e
ollaidh <ollaidh@programmer.net>
2023-09-23 16:55:37 +0300

    Convert pack of strings to pack of ints, create equation

--- eq_lib/eq.cpp
+++ eq_lib/eq.cpp
@@ -1,24 +1,36 @@
-#pragma once
 #include "eq.h"
 #include "lneq.h"
 #include "queq.h"
 #include "cbeq.h"
+#include "utils.h"
 
-std::unique_ptr<Equation> createEquation(std::vector<std::string> input) {
-    int coeffsAmount = input.size();
+std::vector<int> verifyCoeffsPack(const std::vector<std::string>& input) {
+    std::vector<int> result;
+    try {
+        for (const std::string& coeff : input) {
+            int c = stringToInt(coeff);
+            if (!result.empty() || (result.empty() && c != 0)) {
+                result.push_back(c);
+            }
+        }
+        return result;
+    } catch (const std::invalid_argument&)  {
+        throw std::invalid_argument("Wrong input: Non-INT coefficients.");
+    };
+}
+
+std::unique_ptr<Equation> createEquation(const std::vector<int>& coeffs) {
+    std::size_t coeffsAmount = coeffs.size();
 
     if (coeffsAmount == 2) {
-        std::vector<int> coeffs = {1, 2};
         return std::make_unique<LinearEquation>(coeffs);
     } else if (coeffsAmount == 3) {
-        std::vector<int> coeffs = {1, 2, 3};
         return std::make_unique<QuadraticEquation>(coeffs);
     } else if (coeffsAmount == 4) {
-        std::vector<int> coeffs = {1, 2, 3, 4};
         return std::make_unique<CubicEquation>(coeffs);
     } else {
         // TODO change into Raise Error:
-        std::cout << "Bad amount of coeffs: " << coeffsAmount << "\n";
+        std::cout << "Wrong amount of coeffs: " << coeffsAmount << "\n";
         return nullptr;
     }
 }
--- eq_lib/eq.h
+++ eq_lib/eq.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 
 class Equation {
@@ -7,7 +6,10 @@ public:
     Equation() = default;
     virtual ~Equation() = default;
     virtual void solve() = 0;
+    [[nodiscard]] virtual std::vector<int> getRoots() const = 0;
 };
 
-std::unique_ptr<Equation> createEquation(std::vector<std::string> coeffs);
+std::vector<int> verifyCoeffsPack(const std::vector<std::string>& input);
+
+std::unique_ptr<Equation> createEquation(const std::vector<int>& coeffs);
 
