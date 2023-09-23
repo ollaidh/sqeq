4adf89ed171f079e8b0abce95154ba313db746b3
ollaidh <ollaidh@programmer.net>
2023-09-23 16:54:51 +0300

    Fix conversions from string to int

--- eq_lib/utils.cpp
+++ eq_lib/utils.cpp
@@ -5,13 +5,22 @@
 #include <sstream>
 
 // Convert string_view to int:
-int stringviewToInt(std::string_view& strv) {
+int stringviewToInt(std::string_view strv) {
     int result;
     auto conv = std::from_chars(strv.data(), strv.data() + strv.size(), result);
     if (conv.ec == std::errc::invalid_argument || conv.ptr != strv.data() + strv.size()) {
         throw std::invalid_argument("");
     }
     return result;
+}
+
+int stringToInt(const std::string& str) {
+    std::string::const_iterator it = str.begin();
+    while (it != str.end() && std::isdigit(*it)) ++it;
+    if (str.empty() || it != str.end()) {
+        throw std::invalid_argument("");
+    }
+    return std::stoi(str);
 }
 
 // Split string by given separator:
--- eq_lib/utils.h
+++ eq_lib/utils.h
@@ -1,4 +1,5 @@
 #include <string>
 
-int stringviewToInt(std::string_view& strv);
+int stringviewToInt(std::string_view strv);
+int stringToInt(const std::string& str);
 std::vector<std::string> splitString(const std::string& word, const char& separator);
