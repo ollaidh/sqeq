c51b9e307f00353ea06b7791f3890122c4ba1891
ollaidh <ollaidh@programmer.net>
2023-09-23 17:31:36 +0300

    Implement nice parseCoeffs function

--- eq_lib/parse.h
+++ eq_lib/parse.h
@@ -2,20 +2,23 @@
 
 #include <vector>
 #include <string>
+#include <charconv>
 
 template <typename S>
 std::vector<int> parseCoeffs(const std::vector<S>& input)
 {
     std::vector<int> result;
-    try {
-        for (const auto& coeff : input) {
-            int c = 0;
-            if (!result.empty() || (result.empty() && c != 0)) {
-                result.push_back(c);
-            }
+    result.reserve(input.size());
+    for (const auto& coeff : input)
+    {
+        int converted;
+        auto conv = std::from_chars(coeff.data(), coeff.data() + coeff.size(), converted);
+        if (conv.ec == std::errc::invalid_argument || conv.ptr != coeff.data() + coeff.size()) {
+            return {};
         }
-        return result;
-    } catch (const std::invalid_argument&)  {
-        throw std::invalid_argument("Wrong input: Non-INT coefficients.");
-    };
+        if (result.empty() && converted == 0)
+            continue;
+        result.push_back(converted);
+    }
+    return result;
 }
