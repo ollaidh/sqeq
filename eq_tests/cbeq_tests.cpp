d2187887a9d123bf4aef218512da92640bb2e9c6
ollaidh <ollaidh@programmer.net>
2023-09-28 18:38:58 +0300

    Add tests - now for all possible combinations of S, Q

--- eq_tests/cbeq_tests.cpp
+++ eq_tests/cbeq_tests.cpp
@@ -27,6 +27,31 @@ TEST(cbeq, solve) {
     }
 
     {
+        // S < 0, Q > 0
+        CubicEquation eq({1, 2, -3, 1});
+        std::vector<double> expected = {-3.07959562349};
+        eq.solve();
+        std::vector<double> roots = eq.getRoots();
+        ASSERT_EQ(roots.size(), expected.size());
+        for (int i = 0; i < roots.size(); i++) {
+            ASSERT_NEAR(roots[i], expected[i], 1e-8);
+        }
+    }
+
+    {
+        // S < 0, Q == 0
+        CubicEquation eq({1, 3, 3, 0});
+        std::vector<double> expected = {0};
+        eq.solve();
+        std::vector<double> roots = eq.getRoots();
+        ASSERT_EQ(roots.size(), expected.size());
+        for (int i = 0; i < roots.size(); i++) {
+            ASSERT_NEAR(roots[i], expected[i], 1e-8);
+        }
+    }
+
+    {
+        // S == 0
         CubicEquation eq({1, 0, -3, 2});
         std::vector<double> expected = {-2, 1};
         eq.solve();
@@ -37,6 +62,4 @@ TEST(cbeq, solve) {
             ASSERT_NEAR(roots[i], expected[i], 1e-8);
         }
     }
-
-
 }
