2ba3967aadccd35571b18c66f5f21b60ca456e9b
ollaidh <ollaidh@programmer.net>
2023-10-02 21:27:17 +0300

    Add base class Reader for classes CommandlineReader and FileReader

--- eq_lib/cmd_reader.h
+++ eq_lib/cmd_reader.h
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "reader.h"
 
-class CommandlineReader {
+class CommandlineReader : public Reader{
 public:
     CommandlineReader(int argc, const char ** arguments);
-    std::vector<std::string> getCoeffs();
+    std::vector<std::string> getCoeffs() override;
 private:
     const char ** m_argv;
     int m_argc;
--- eq_lib/file_reader.h
+++ eq_lib/file_reader.h
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include "reader.h"
 
-class FileReader {
+class FileReader : Reader {
 public:
-    FileReader(std::istream* stream);
-    std::vector<std::string> getCoeffs();
+    explicit FileReader(std::istream* stream);
+    std::vector<std::string> getCoeffs() override;
 private:
     std::istream* m_stream;
 };
