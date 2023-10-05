2c4b4560cec13afb1344395a5cf245bd509e1d3c
ollaidh <ollaidh@programmer.net>
2023-10-05 13:56:28 +0300

    Add try to catch if file we read does not exist

--- eq_app/main.cpp
+++ eq_app/main.cpp
@@ -6,25 +6,28 @@
 #include "printer.h"
 
 int main(int argc, const char * argv[]) {
-    auto reader = createReader(argc, argv);
-    int i = 0;
-    while (!reader->atEnd()) {
-        auto currInput = reader->getCoeffs();
-        auto coeffs = parseCoeffs(currInput);
-        if (coeffs.empty()) {
-            std::cout << "Bad coeffs pack ";
-            print_sequence(currInput, std::cout);
-            std::cout <<  "! Skipping this equation.";
+    try {
+        auto reader = createReader(argc, argv);
+        int i = 0;
+        while (!reader->atEnd()) {
+            auto currInput = reader->getCoeffs();
+            auto coeffs = parseCoeffs(currInput);
+            if (coeffs.empty()) {
+                std::cout << "Bad coeffs pack ";
+                print_sequence(currInput, std::cout);
+                std::cout <<  "! Skipping this equation.";
+                std::cout << "\n";
+                continue;
+            }
+            auto eq = createEquation(coeffs);
+            eq->solve();
+            auto roots = eq->getRoots();
+            printEq(coeffs, roots, std::cout);
             std::cout << "\n";
-            continue;
+            i++;
         }
-        auto eq = createEquation(coeffs);
-        eq->solve();
-        auto roots = eq->getRoots();
-        printEq(coeffs, roots, std::cout);
-        std::cout << "\n";
-        i++;
-
+    } catch (std::invalid_argument& e) {
+        std::cout << e.what();
     }
 
     return 0;
