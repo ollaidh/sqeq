e22bce7ed5d7e04470f56120dc27723841253ace
ollaidh <ollaidh@programmer.net>
2023-10-09 20:34:15 +0300

    Add test for reading from command line (case with more args then required for certain equation type)

--- eq_tests/cmd_reader_tests.cpp
+++ eq_tests/cmd_reader_tests.cpp
@@ -3,7 +3,7 @@
 #include "cmd_reader.h"
 
 TEST(cmdReader, getCoeffs) {
-    const char * argv[] = {"path", "Q", "1", "2", "3", "L", "4", "5", "C", "11", "12"};
+    const char * argv[] = {"path", "Q", "1", "2", "3", "4", "L", "4", "5", "C", "11", "12"};
     CommandlineReader reader(11, argv);
 
     auto resultCall_1 = reader.getCoeffs();
@@ -12,14 +12,19 @@ TEST(cmdReader, getCoeffs) {
     ASSERT_EQ(resultCall_1, expectedCall_1);
 
     auto resultCall_2 = reader.getCoeffs();
-    ASSERT_EQ(resultCall_2.size(), 2);
-    std::vector<std::string> expectedCall_2 = {"4", "5"};
+    ASSERT_TRUE(resultCall_2.empty());
+    std::vector<std::string> expectedCall_2 = {};
     ASSERT_EQ(resultCall_2, expectedCall_2);
 
     auto resultCall_3 = reader.getCoeffs();
-    ASSERT_TRUE(resultCall_3.empty());
-    std::vector<std::string> expectedCall_3 = {};
+    ASSERT_EQ(resultCall_3.size(), 2);
+    std::vector<std::string> expectedCall_3 = {"4", "5"};
     ASSERT_EQ(resultCall_3, expectedCall_3);
+
+    auto resultCall_4 = reader.getCoeffs();
+    ASSERT_TRUE(resultCall_4.empty());
+    std::vector<std::string> expectedCall_4 = {};
+    ASSERT_EQ(resultCall_4, expectedCall_4);
 
     ASSERT_TRUE(reader.atEnd());
 
